9e6e2c223b497365b64f754b84152362800f5e06
Gurupratap CR <gurupratap.cr@volantetech.com>
2024-02-26 14:20:13 +0530

    Spring Boot Movie aplication

---
+++ src/main/java/dev/igpk/movies/MovieController.java
@@ -0,0 +1,27 @@
+package dev.igpk.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getMovies() {
+        return new ResponseEntity<>(movieService.findAllMovies(), HttpStatus.OK);
+    }
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.findMovieByImdbId(imdbId), HttpStatus.OK);
+    }
+}
+
